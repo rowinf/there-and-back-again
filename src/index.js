cf4809f98cfcb8c1cb542feb63fba767648fb8b9
Rob <irwin.shell@gmail.com>
2020-06-26 17:35:07 +1200

    added first mapbox map

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+const map = new mapboxgl.Map({
+  container: 'root',
+  style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+  center: [174.6214276,-41.2442847], // starting position [lng, lat]
+  zoom: 9 // starting zoom
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
